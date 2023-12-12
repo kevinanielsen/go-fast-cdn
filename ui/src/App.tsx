453aa2a1ef58a3db4bc622bf88e1b3e128e1c79b
kevinanielsen <keveran@gmail.com>
2023-12-12 09:16:43 +0100

    feat(ui): build frontpage

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,48 @@
 import { Link, Route } from "wouter";
+import { Toaster } from "react-hot-toast";
+import Files from "./components/files";
+import { Image, Upload as UploadIcon, Files as FilesIcon } from "lucide-react";
+import Seperator from "./components/seperator";
+import Upload from "./components/upload";
 
 function App() {
   return (
     <>
-      <nav></nav>
-      <main>
-        <Link to="/images">Images</Link>
-        <Route path="/images">About Us</Route>
-      </main>
+      <Toaster />
+      <div className="flex h-screen w-screen">
+        <nav className="min-w-[256px] min-h-screen h-full border-r shadow-lg p-4 flex flex-col">
+          <h1 className="text-xl font-bold">Go-Fast CDN</h1>
+          <Seperator />
+          <Link to="/upload" className="flex font-bold gap-4 items-center">
+            <UploadIcon />
+            Upload Content
+          </Link>
+          <Seperator />
+          <h3 className="text-lg mb-4 font-bold">Content</h3>
+          <ul className="flex flex-col gap-4">
+            <li>
+              <Link to="/images" className="flex font-bold gap-4 items-center">
+                <Image />
+                Images
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/documents"
+                className="flex font-bold gap-4 items-center"
+              >
+                <FilesIcon />
+                Documents
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <main className="m-4">
+          <Route path="/images">{<Files type="images" />}</Route>
+          <Route path="/documents">{<Files type="documents" />}</Route>
+          <Route path="/upload">{<Upload />}</Route>
+        </main>
+      </div>
     </>
   );
 }
