36480a447f32b3cb3586eb4ec118a13be449c568
kevinanielsen <keveran@gmail.com>
2023-12-13 15:32:26 +0100

    chore: add buttons and types

--- ui/src/components/content-card.tsx
+++ ui/src/components/content-card.tsx
@@ -1,17 +1,19 @@
-import { FileText, Files } from "lucide-react";
+import { DownloadCloud, FileText, Files } from "lucide-react";
 import { toast } from "react-hot-toast";
 
 type TContentCardProps = {
-  file_name: string;
-  type: "images" | "documents";
-  ID: number;
-  createdAt: Date;
-  updatedAt: Date;
+  file_name?: string;
+  type?: "images" | "documents";
+  ID?: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+  disabled?: boolean;
 };
 
 const ContentCard: React.FC<TContentCardProps> = ({
   file_name,
-  type,
+  type = "documents",
+  disabled = false,
   // ID,
   // createdAt,
   // updatedAt,
@@ -21,30 +23,42 @@ const ContentCard: React.FC<TContentCardProps> = ({
   }/api/cdn/download/${type === "documents" ? "docs" : "images"}/${file_name}`;
 
   return (
-    <div className="border rounded-lg shadow-lg flex flex-col w-64 max-w-[256px] justify-center items-center gap-4 p-4">
+    <div className="border rounded-lg shadow-lg flex flex-col min-h-[264px] w-64 max-w-[256px] justify-center items-center gap-4 p-4">
       {type === "images" ? (
         <img
           src={url}
           alt={file_name}
-          width={200}
+          width={224}
           height={150}
-          className="object-cover max-h-[150px] max-w-[200px]"
+          className="object-cover max-h-[150px] max-w-[224px]"
         />
       ) : (
-        <FileText />
+        <FileText size="128" />
       )}
       <p className="truncate w-64 px-4">{file_name}</p>
-      <div className="flex">
+      <div className={`flex gap-2 w-full ${disabled && "sr-only"}`}>
         <button
-          className="flex justify-center items-center text-sky-600"
+          className="flex justify-center items-center text-sky-600 tooltip"
           onClick={() => {
             navigator.clipboard.writeText(url);
             toast.success("clipboard saved");
           }}
+          aria-label="Copy Link"
+          aria-labelledby="Copy Link"
         >
-          <Files className="inline" size="20" />
-          Copy Link
+          <span className="tooltiptext">Copy Link</span>
+          <Files className="inline" size="24" />
         </button>
+        <a
+          className="flex justify-center items-center text-sky-600 tooltip"
+          aria-label="Download file"
+          aria-labelledby="Download file"
+          href={url}
+          download
+        >
+          <span className="tooltiptext">Download file</span>
+          <DownloadCloud className="inline" size="24" />
+        </a>
       </div>
     </div>
   );
