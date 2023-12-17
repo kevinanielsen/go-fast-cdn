08eabfcd86b5f460ea128563bc1e924970c85c5c
Kevin Nielsen <kevinanielsen@outlook.com>
2023-12-17 11:02:31 +0100

    docs: add docs with astro/starlight (#13)

---
+++ docs/astro.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Go-fast CDN",
+      social: {
+        github: "https://github.com/kevinanielsen/go-fast-cdn",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Usage", link: "/guides/usage/" },
+            { label: "Hosting", link: "/guides/hosting/" },
+          ],
+        },
+      ],
+    }),
+  ],
+});
