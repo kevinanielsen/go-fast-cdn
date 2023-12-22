18fb57b4a7ae3711e5600aa5822fcbb17a834c96
kevinanielsen <keveran@gmail.com>
2023-12-22 21:11:32 +0100

    docs: add documentation for router.go

--- router/router.go
+++ router/router.go
@@ -8,6 +8,8 @@ import (
 	"github.com/kevinanielsen/go-fast-cdn/ui"
 )
 
+// Router initializes the router and sets up middleware, routes, etc.
+// It returns a *gin.Engine instance configured with the routes, middleware, etc.
 func Router() {
 	r := gin.Default()
 
