aeab18b87b26c91c60233511f4099f9e9467eacb
kevinanielsen <keveran@gmail.com>
2023-12-22 21:05:12 +0100

    docs: add documentation for filterFilename.go

--- util/filterFilename.go
+++ util/filterFilename.go
@@ -1,3 +1,4 @@
+// Package util contains utility functions used throughout the application.
 package util
 
 import (
@@ -5,6 +6,10 @@ import (
 	"strings"
 )
 
+// countVal counts the number of occurrences of val in str.
+// It splits str into a slice of strings, iterates over the slice
+// checking each string for equality with val, and increments a counter
+// each time a match is found. The final count is returned.
 func countVal(str string, val string) int {
 	var count int
 	arr := strings.Split(str, "")
@@ -17,6 +22,10 @@ func countVal(str string, val string) int {
 	return count
 }
 
+// FilterFilename removes illegal characters from a filename string.
+// It ensures there is at most one period in the filename,
+// replaces any '/' and '\' characters,
+// and returns the filtered string.
 func FilterFilename(filename string) (string, error) {
 	if countVal(filename, ".") > 1 {
 		return filename, errors.New("filename cannot contain more than one period character")
