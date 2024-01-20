ab19a683ee8902461f4e2f64e0e2cb77764d61d1
Kevin Nielsen <kevinanielsen@outlook.com>
2024-01-20 20:19:04 +0100

    feat: implement shadcn/ui

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
   server: {
     proxy: {
       "/api/": {
