00c778dedc240424bff39b7fab56b6bd5ce93963
Omar Minaya <omiinaya@gmail.com>
2025-06-21 13:22:26 -0400

    removed unnecessary test file.

--- clear-browser-data.js
+++
@@ -1,9 +0,0 @@
-// Run this in the browser console to clear all auth data
-localStorage.removeItem('accessToken');
-localStorage.removeItem('refreshToken');
-localStorage.removeItem('user');
-console.log('All auth data cleared from localStorage');
-
-// Or clear everything
-// localStorage.clear();
-// console.log('All localStorage data cleared');
