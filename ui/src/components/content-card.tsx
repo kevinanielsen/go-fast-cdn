254ed99b813767cbd25b8be45675ae8f44bc545a
Muhammad Azka <hello.mazka@gmail.com>
2025-06-26 11:00:56 +0700

    refactor: update UI components and styles for improved consistency and functionality
    - Refactored PostCSS configuration to use '@tailwindcss/postcss' plugin.
    - Modified ContentCard component to adjust class names for better accessibility.
    - Enhanced Button component with new variants and improved class management.
    - Updated Dialog component structure for better readability and added close button functionality.
    - Refined Input component styling and structure for consistency.
    - Improved Label component with better class handling.
    - Reorganized global CSS styles, including the removal of Tailwind's default directives.
    - Deleted outdated Tailwind configuration file as part of the CSS restructuring.

--- ui/src/components/content-card.tsx
+++ ui/src/components/content-card.tsx
@@ -63,7 +63,7 @@ const ContentCard: React.FC<TContentCardProps> = ({
               <Files className="inline" size="24" />
             </Button>
             <a
-              className="hover:bg-accent hover:text-accent-foreground h-10 w-10 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-sky-600"
+              className="hover:bg-accent hover:text-accent-foreground h-10 w-10 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-sky-600"
               aria-label="Download file"
               href={url}
               download
