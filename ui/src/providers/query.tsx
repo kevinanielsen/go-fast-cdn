94019a059087c93a9ee08c0b38f9c345b9f7b9f5
Muhammad Azka <hello.mazka@gmail.com>
2025-06-29 00:52:27 +0700

    refactor(query): remove React Query Devtools from QueryProvider

--- ui/src/providers/query.tsx
+++ ui/src/providers/query.tsx
@@ -3,7 +3,6 @@
 import { useState } from "react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
@@ -18,9 +17,6 @@ export function QueryProvider({ children }: { children: React.ReactNode }) {
   );
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
