a4b13b8ba7c955d054388ea360bae6fe3b387ab1
Muhammad Azka <hello.mazka@gmail.com>
2025-07-09 01:02:23 +0700

    feat(user-management): enhance user management page functionality
    - Added current user context to manage user-specific actions
    - Updated columns definition for better role and verification display
    - Improved action buttons to restrict editing and deletion for current user

--- ui/src/components/modules/master-data/user-management/user-management-page.tsx
+++ ui/src/components/modules/master-data/user-management/user-management-page.tsx
@@ -20,9 +20,11 @@ import useRegistrationEnabledQuery from "./hooks/use-registration-enabled-query"
 import UseToggleRegistrationMutation from "./hooks/use-toggle-registration-mutation";
 import toast from "react-hot-toast";
 import useUsersQuery from "./hooks/use-users-query";
+import { useAuth } from "@/contexts/AuthContext";
 
 const UserManagementPage = () => {
   const { data } = useUsersQuery();
+  const { user: currentUser } = useAuth();
   const registrationEnabledQuery = useRegistrationEnabledQuery();
   const registrationToggleMutation = UseToggleRegistrationMutation();
 
@@ -35,115 +37,126 @@ const UserManagementPage = () => {
   const [search, setSearch] = useState("");
   const [debounceSearch, setDebounceSearch] = useState("");
 
-  const columns: ColumnDef<TUser>[] = [
-    {
-      header: "No. ",
-      accessorFn: (_row, index) => index + 1,
-      id: "no",
-    },
-    // email
-    {
-      header: "Email",
-      accessorKey: "email",
-    },
-    // role
-    {
-      header: "Role",
-      accessorKey: "role",
-      cell: ({ getValue }) => {
-        const role = getValue();
-        return (
-          <span
-            className={`px-2 py-1 rounded-full text-xs font-bold ${
-              role === "admin"
-                ? "bg-blue-100 text-blue-700"
-                : "bg-gray-200 text-gray-700"
-            }`}
-          >
-            {role as string}
-          </span>
-        );
+  const columns = useMemo<ColumnDef<TUser>[]>(
+    () => [
+      {
+        header: "No. ",
+        accessorFn: (_row, index) => index + 1,
+        id: "no",
       },
-    },
-    // is_verified
-    {
-      header: "Verified",
-      accessorKey: "is_verified",
-      cell: ({ getValue }) => (
-        <div>
-          <span
-            className={`inline-block w-3 h-3 rounded-full ${
-              getValue() ? "bg-green-400" : "bg-red-400"
-            }`}
-          ></span>
-          <span className="ml-2">{getValue() ? "Yes" : "No"}</span>
-        </div>
-      ),
-    },
-    // created at
-    {
-      header: "Created At",
-      accessorKey: "created_at",
-      cell: ({ getValue }) => {
-        const date = new Date(getValue() as string);
-        return date.toLocaleDateString("en-US", {
-          year: "numeric",
-          month: "short",
-          day: "numeric",
-        });
+      // email
+      {
+        header: "Email",
+        accessorKey: "email",
       },
-    },
-    // last login
-    {
-      header: "Last Login",
-      accessorKey: "last_login",
-      cell: ({ getValue }) => {
-        if (getValue() === null) {
-          return <span className="text-gray-500">Never</span>;
-        }
-        const date = new Date(getValue() as string);
-        return date.toLocaleDateString("en-US", {
-          year: "numeric",
-          month: "short",
-          day: "numeric",
-        });
-      },
-    },
-    {
-      header: "Actions",
-      id: "actions",
-      cell: ({ row }) => {
-        const userData = row.original;
-        return (
-          <div className="flex gap-2">
-            <Button
-              onClick={() => {
-                setMode("edit");
-                setSelectedData(userData);
-                setFormModalOpen(true);
-              }}
-              variant="outline"
-              size="icon"
+      // role
+      {
+        header: "Role",
+        accessorKey: "role",
+        cell: ({ getValue }) => {
+          const role = getValue();
+          return (
+            <span
+              className={`px-2 py-1 rounded-full text-xs font-bold ${
+                role === "admin"
+                  ? "bg-blue-100 text-blue-700"
+                  : "bg-gray-200 text-gray-700"
+              }`}
             >
-              <Pencil />
-              <span className="sr-only">Edit</span>
-            </Button>
-            <Button
-              variant="destructive"
-              size="icon"
-              onClick={() => {
-                setSelectedData(userData);
-                setDeleteConfirmModalOpen(true);
-              }}
-            >
-              <Trash />
-              <span className="sr-only">Delete</span>
-            </Button>
+              {role as string}
+            </span>
+          );
+        },
+      },
+      // is_verified
+      {
+        header: "Verified",
+        accessorKey: "is_verified",
+        cell: ({ getValue }) => (
+          <div>
+            <span
+              className={`inline-block w-3 h-3 rounded-full ${
+                getValue() ? "bg-green-400" : "bg-red-400"
+              }`}
+            ></span>
+            <span className="ml-2">{getValue() ? "Yes" : "No"}</span>
           </div>
-        );
+        ),
       },
-    },
-  ];
+      // created at
+      {
+        header: "Created At",
+        accessorKey: "created_at",
+        cell: ({ getValue }) => {
+          const date = new Date(getValue() as string);
+          return date.toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          });
+        },
+      },
+      // last login
+      {
+        header: "Last Login",
+        accessorKey: "last_login",
+        cell: ({ getValue }) => {
+          if (getValue() === null) {
+            return <span className="text-gray-500">Never</span>;
+          }
+          const date = new Date(getValue() as string);
+          return date.toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          });
+        },
+      },
+      {
+        header: "Actions",
+        id: "actions",
+        cell: ({ row }) => {
+          const userData = row.original;
+          return (
+            <div className="flex gap-2">
+              {currentUser && currentUser.id === userData.id ? (
+                <span className="text-gray-500 cursor-not-allowed">
+                  It's you!
+                </span>
+              ) : (
+                <>
+                  <Button
+                    onClick={() => {
+                      setMode("edit");
+                      setSelectedData(userData);
+                      setFormModalOpen(true);
+                    }}
+                    variant="outline"
+                    size="icon"
+                  >
+                    <Pencil />
+                    <span className="sr-only">Edit</span>
+                  </Button>
+                  <Button
+                    variant="destructive"
+                    size="icon"
+                    onClick={() => {
+                      setSelectedData(userData);
+                      setDeleteConfirmModalOpen(true);
+                    }}
+                  >
+                    <Trash />
+                    <span className="sr-only">Delete</span>
+                  </Button>
+                </>
+              )}
+            </div>
+          );
+        },
+      },
+    ],
+    [currentUser]
+  );
 
   const defineButtonState = useMemo(() => {
     if (registrationEnabledQuery.isLoading) {
